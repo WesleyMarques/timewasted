2687fecb75af7e0c5f0720a635caaab7f4f1e6e9
Gustavo Yamaguchi <gustavoyama@gmail.com>
2015-04-29 03:02:11 -0300

    adicionando o HighCharts

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'firebase'])
+angular.module('starter', ['ionic', 'firebase', 'highcharts-ng'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -28,6 +28,15 @@ angular.module('starter', ['ionic', 'firebase'])
     templateUrl: "templates/menu.html",
     controller: 'MenuController'
   })
+
+      .state('app.grafico', {
+        url: "/grafico",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/grafico.html"
+          }
+        }
+      })
 
   .state('login', {
     url: "/login",
