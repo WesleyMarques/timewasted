81fd99fa9f18911b412580fd9abb59c9a5334d25
WesleyMarques <wesley.nmtorres@gmail.com>
2015-05-13 20:47:54 -0300

    test gulp in travis

--- tests/Controllers/controllers.tests.js
+++ tests/Controllers/controllers.tests.js
@@ -1,16 +1,16 @@
 'use strict';
 describe('Controllers', function(){
-    var scope;
+    var scope,ctrl;
 
     // load the controller's module
     beforeEach(angular.mock.module('app'));  
     beforeEach(angular.mock.inject(function($rootScope, $controller) {
         scope = $rootScope.$new();
-        $controller('CadastroController', {$scope: scope});
+        ctrl =  $controller('CadastroController', {$scope: scope});
     }));  
 
     it('should have all Controllers started',function(){
-        expect(true).toBe(true);
+        expect(ctrl).toBeDefined();
     });
     
 });
